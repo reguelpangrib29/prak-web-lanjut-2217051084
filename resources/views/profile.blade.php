2c5e7d926da14e794f2d42ff81e46a53ba3bf44c
ASUS <reguelpangaribuan2@gmail.com>
2024-09-06 17:09:24 +0700

    pesan commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Ini Halaman Profile</h1>
+
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td><?= $nama ?></td><br>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>:</td>
+        <td><?= $kelas ?></td><br>
+    </tr>
+    <tr>
+        <td>NPM</td>
+        <td>:</td>
+        <td><?= $npm ?></td><br>
+    </tr>
+</body>
+
+</html>
