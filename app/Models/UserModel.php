859ea7eb9591e27bba8fdd5f9775aff7ca688c43
ASUS <reguelpangaribuan2@gmail.com>
2024-10-10 15:17:51 +0700

    Pembaruan 6

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,13 +12,27 @@ class UserModel extends Model
     protected $table = 'user';
     protected $guarded = ['id'];
 
-    public function getUser() {
-        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
-        ->select('user.*', 'kelas.nama_kelas as nama_kelas')
-        ->orderBy('user.id', 'asc')
-        ->get();
+    protected $fillable = [
+        'nama',
+        'npm',
+        'kelas_id',
+        'foto',
+    ];
 
-    }
+    public function getUser($id = null) {
+        if ($id !== null) {
+            return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                        ->select('user.*', 'kelas.nama_kelas as nama_kelas')
+                        ->where('user.id', $id)
+                        ->first(); // Menggunakan first() untuk mengambil satu record
+        }
+        
+        // Jika $id null, maka kembalikan seluruh data user
+        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                    ->select('user.*', 'kelas.nama_kelas as nama_kelas')
+                    ->orderBy('user.id', 'asc')
+                    ->get();
+    }    
 
     public function kelas ()
     {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List User</title>
+    <title>{{ $title ?? 'Edit User' }}</title>
     @vite('resources/css/app.css')
 </head>
 
